840ca7e8b6cf9889279a20211aa6ad033ebcb1b6
UnnatiDutt <71591175+UnnatiDutt@users.noreply.github.com>
2024-03-12 16:48:48 +0530

    Update transaction.js

--- server/models/transaction.js
+++ server/models/transaction.js
@@ -1,9 +1,28 @@
 const mongoose = require("mongoose");
 
 const transactionSchema = new mongoose.Schema({
-  // ...
-  // ...
-  // ...
+  applicableItems: {
+   type : Array,
+    required : [ture,"Atleast one item is required"],
+  },
+  totalPrices : {
+    type : Double,
+    required : [true,"totalPrices is required " ],
+      default : 0,
+  }
+totalDiscount : {
+    type : Double,
+    required : false,
+     }
+createdAt : {
+    type : Date,
+    required : true,
+      default : Date.now,
+    }
+updatedAt : {
+  type : Date,
+    required : true,
+}
 });
 
 module.exports = mongoose.model("Transaction", transactionSchema);
