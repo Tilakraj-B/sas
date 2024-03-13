5efac3352848dcd4ed260ec25c139d2312865838
UnnatiDutt <71591175+UnnatiDutt@users.noreply.github.com>
2024-03-13 18:53:40 +0530

    Update transactions.js

--- server/controllers/transactions.js
+++ server/controllers/transactions.js
@@ -1,6 +1,23 @@
+
+const Transaction = require("./models/Transaction");
+
 class TransactionsController {
   async create(req, res, next) {
-    // ...
+    const { sales, totalPrice, totalDiscount } = req.body;
+
+    try {
+      const transaction = new Transaction({
+        sales,
+        totalPrice,
+        totalDiscount
+      });
+
+      await transaction.save();
+
+      return res.status(201).json(transaction);
+    } catch (error) {
+      next(error);
+    }
   }
 }
 
